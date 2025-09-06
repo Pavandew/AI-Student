381d2ae5cf104bc67c51940230952a0ddad1cb35
Pavandew <pavandewangan4@gmail.com>
2025-09-06 19:00:41 +0530

    remove api key

--- app/src/main/java/com/ourmindai/aistudent/Constants.kt
+++ app/src/main/java/com/ourmindai/aistudent/Constants.kt
@@ -1,10 +1,7 @@
 package com.ourmindai.aistudent
 
 object Constants {
-//    val API_KEY2 = "AIzaSyA-Ef9cXxmggfxI8D9m4vdj3nNs_UIfnyQ"
-    val API_KEY = "AIzaSyBmdqTbVe-44EINIwhvS9M0x8exNwhMCcw"  // Gemini
-
-    val MISTRAL_API_KEY = "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"
+//    val API_KEY = "AIzaSyA-Ef9cXxmggfxI8D9m4vdj3nNs_UIfnyQ"
 
     val OPEN_AI_API_KEY = "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"
 
